6b96eba68325cd069236565160080466a72356d6
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-03 21:34:56 +0500

    footer text changed

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -130,10 +130,11 @@
         </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy; 2022-{{now()->format('Y')}} <a
+                    href="https://t.me/abdurashid_coder">Abdurashid</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 0.0.1
             </div>
         </footer>
 
