7b23342e1516298de64e5bc38601f31fd4e3f0be
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-03 11:50:41 +0500

    admin lte added

---
+++ resources/views/admin/layouts/menus.blade.php
@@ -0,0 +1,37 @@
+<li class="nav-item">
+    <a href="{{route('admin')}}" class="nav-link @if (request()->is('admin')) active @endif">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+            Dashboard
+        </p>
+    </a>
+</li>
+{{-- <li class="nav-item menu-open">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+            Dashboard
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="./index.html" class="nav-link active">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v1</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./index2.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v2</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./index3.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v3</p>
+            </a>
+        </li>
+    </ul>
+</li> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
