0d4b1fa868d4c246fb6261ff920dc24914ac47c7
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-05 18:32:29 +0500

    some components added

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h3 class="m-0 text-dark">{{$title}}</h3>
+    </div>
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><i class="fas fa-home"></i> {{__('words.dashboard')}}</a></li>
+            <li class="breadcrumb-item active"><i class="{{$icon}}"></i> {{$title}}</li>
+        </ol>
+    </div>
+</div>
