59f4f5b3c0b8c12e5887d4578a5504d61e54eeaf
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-07 16:43:07 +0500

    projects show done

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,36 +1,43 @@
 @extends('admin.layouts.app')
 @section('title')
-    Show work
+    Show project
 @endsection
 @section('content')
-    <x-headers title="{{$work->title}}" icon="fas fa-eye" parent="Works" parentIcon="fas fa-laptop-house"
-               parentRoute="admin.works.index"/>
+    <x-headers title="{{$project->title}}" icon="fas fa-eye" parent="Projects" parentIcon="fas fa-trophy"
+               parentRoute="admin.projects.index"/>
     <div class="card">
         <div class="card-header">
-            <a href="{{route('admin.works.edit', $work)}}" class="btn btn-success"><i class="fas fa-pencil-alt"></i> Edit</a>
-            <form action="{{route('admin.works.destroy', $work)}}" class="d-inline-block" method="POST">
+            <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-success"><i
+                    class="fas fa-pencil-alt"></i> Edit</a>
+            <form action="{{route('admin.projects.destroy', $project)}}" class="d-inline-block" method="POST">
                 @csrf
                 @method('DELETE')
-                <button class="btn btn-danger" type="submit" onclick="return confirm('Are you sure man???')"><i class="fas fa-trash"></i> Delete</button>
+                <button class="btn btn-danger" type="submit" onclick="return confirm('Are you sure man???')"><i
+                        class="fas fa-trash"></i> Delete
+                </button>
             </form>
         </div>
         <div class="card-body table-responsive">
             <table class="table table-bordered">
                 <tr>
                     <th>ID</th>
-                    <td>{{$work->id}}</td>
+                    <td>{{$project->id}}</td>
                 </tr>
                 <tr>
                     <th>Title</th>
-                    <td>{{$work->title}}</td>
+                    <td>{{$project->title}}</td>
                 </tr>
                 <tr>
-                    <th>Time interval</th>
-                    <td>{{$work->time}}</td>
+                    <th>Live demo</th>
+                    <td><a href="{{$project->demo_url}}">{{$project->demo_url}}</a></td>
                 </tr>
                 <tr>
-                    <th>Comment</th>
-                    <td>{!! $work->desc !!}</td>
+                    <th>Source code</th>
+                    <td><a href="{{$project->code_url}}">{{$project->code_url}}</a></td>
+                </tr>
+                <tr>
+                    <th>Description</th>
+                    <td>{!! $project->desc !!}</td>
                 </tr>
             </table>
         </div>
